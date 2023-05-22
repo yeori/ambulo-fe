6a25b39688b05b71b27f6a998ecd137ae1949b42
yeori <yeori.seo@gmail.com>
2023-05-22 14:10:58 +0900

    router 구현

--- src/apps/main/MainApp.svelte
+++ src/apps/main/MainApp.svelte
@@ -2,27 +2,31 @@
   import page from 'page'
   import '../../assets/global.scss'
   import 'bootstrap/scss/bootstrap-grid.scss'
-  import { MainPage, MapPage, LoginPage } from './page/index.js'
+  import {
+    MainPage,
+    RegionPage,
+    JourneyThemePage,
+    MapPage,
+    LoginPage
+  } from './page/index.js'
   import SplashView from './component/SplashView.svelte'
   import ToastView from '@/common/toast/ToastView.svelte'
-  import MainNav from './component/nav/MainNav.svelte'
+  import GnbWrapper from './component/nav/GnbWrapper.svelte'
   import { fade } from 'svelte/transition'
   import { appConfigDao } from '../../common/entity/index.js'
   import { onMount } from 'svelte'
   import { Debounce } from '@/service/util/debounce.js'
   import { uiState } from '@main/domain/ui/index.js'
-
-  // import Background from "./component/Background.svelte";
-  // import LoginPage from "./page/LoginPage.svelte";
+  import router from '@common/router/index.js'
 
   let showSplash = undefined
-  let routerView = MainPage
-
   // console.log(db)
   const initPagination = () => {
-    page('/', () => (routerView = MainPage))
-    page('/map', () => (routerView = MapPage))
-    page('/login', () => (routerView = LoginPage))
+    router.bind('/', MainPage)
+    router.bind('/map', MapPage)
+    router.bind('/region', RegionPage)
+    router.bind('/login', LoginPage)
+    router.bind('/theme/:themeId', JourneyThemePage)
     page.start()
   }
   initPagination()
@@ -42,13 +46,15 @@
   })
 </script>
 
-<main class="abs-fill" transition:fade>
+<main transition:fade>
   {#if showSplash === true}
     <SplashView on:skip={() => (showSplash = false)} />
   {:else if showSplash === false}
-    <div class="inner abs-fill" in:fade={{ duration: 150, delay: 350 }}>
-      <MainNav />
-      <svelte:component this={routerView} />
+    <div class="inner" in:fade={{ duration: 150, delay: 350 }}>
+      <GnbWrapper {router} />
+      {#key $router.params}
+        <svelte:component this={$router.component} {router} />
+      {/key}
     </div>
   {/if}
 
@@ -57,10 +63,6 @@
 
 <style lang="scss">
   main {
-    position: absolute;
-    inset: 0;
-    width: 100%;
-    height: 100%;
     .inner {
       z-index: 10;
     }
---
+++ src/common/router/index.ts
@@ -0,0 +1,60 @@
+import util from '@/service/util/index.js'
+import page from 'page'
+import { writable, type Subscriber, type Writable, get } from 'svelte/store'
+const update = util.svelteStore.helpUpdate
+
+const routeMiddleware = (router: Router, ctx: any, next: Function) => {
+  update(router.store, (store) => {
+    store.params = ctx.params
+    store.path = ctx.path
+  })
+  next()
+}
+
+export type IRouter = {
+  path: string
+  params: any
+  component: any
+}
+export class Router {
+  store: Writable<IRouter>
+  constructor() {
+    this.store = writable({
+      path: undefined,
+      params: undefined,
+      component: undefined
+    })
+  }
+  push(path: string) {
+    page.show(path)
+  }
+  intParam(paramName) {
+    return this.getParam(paramName, (v) => Number.parseInt(v))
+  }
+  getParam(paramName, conv: Function) {
+    const { params } = get(this.store)
+    const v = params[paramName]
+    return conv(v)
+  }
+  subscribe(callback: Subscriber<IRouter>) {
+    return this.store.subscribe(callback)
+  }
+  bind(path: string, component: any) {
+    page(
+      path,
+      (ctx: page.Context, next) => routeMiddleware(this, ctx, next),
+      () => {
+        update(this.store, (store) => {
+          store.component = component
+        })
+      }
+    )
+  }
+  start() {
+    page.start()
+  }
+}
+
+const createRouter = () => new Router()
+const router = createRouter()
+export default router
