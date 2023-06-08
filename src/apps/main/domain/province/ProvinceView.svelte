987f37e00c8c0bd51dbe9e506a581d745b4c1a38
yeori <yeori.seo@gmail.com>
2023-06-08 14:30:36 +0900

    commit

--- src/apps/main/domain/province/ProvinceView.svelte
+++ src/apps/main/domain/province/ProvinceView.svelte
@@ -4,81 +4,74 @@
   import JourneyCardView from '@/common/entity/journey/JourneyCardView.svelte'
   import { fade } from 'svelte/transition'
   import { createEventDispatcher, onDestroy } from 'svelte'
-  import { TabModel } from '@/common/tab/tab-model.js'
   import TabNavView from '@/common/tab/TabNavView.svelte'
-  import {
-    PlacePagination,
-    placeStore
-  } from '@/common/entity/place/place-store.js'
+  import { placeStore } from '@/common/entity/place/place-store.js'
+  import { festivalStore } from '@/common/entity/festival/festival-store.js'
   import PlaceCardView from '@/common/entity/place/PlaceCardView.svelte'
   import { uiState } from '../ui/index.js'
   import type { Province } from './Province.js'
   import { sheetStore } from '../../component/sheet/index.js'
   import AppButton from '@/common/form/AppButton.svelte'
+  import { type TabData, regionTabModel } from '@/common/tab/tab-instance.js'
+  import type { Festival } from '@/common/entity/festival/Festival.js'
+  import { Place } from '@/common/entity/place/Place.js'
+  import type { Pagination } from '@/service/pagination/Pagination.js'
 
   export let province: Province
-  class TabNav {
-    constructor(readonly id: string, readonly text: string) {}
-  }
-
-  const tabs = [
-    new TabNav('route', '코스'),
-    new TabNav('place', '관광지'),
-    new TabNav('festival', '행사')
-  ]
-
-  const tabModel: TabModel<TabNav> = TabModel.create(
-    tabs,
-    (tab) => tab.id,
-    (tab) => tab.text
-  )
   let viewport = [0, 0]
   const dispatch = createEventDispatcher()
 
   let journeys: Journey[] = []
-  let paging: PlacePagination = undefined
-
+  let paging: Pagination<Place | Festival> = undefined
+  let activeTab: TabData
   const flyOption = { x: -150, duration: 450, opacity: 0.0 }
   $: {
     journeyService.findJourneysByRegion(province.region).then((elem) => {
       journeys = elem
-    })
-    placeStore.loadPlaces(undefined, province.region.regionName).then(() => {
-      if (paging) {
-        paging.reset(
-          $placeStore.visibles.filter((place) => place.type !== 'P15')
-        )
-      }
     })
   }
   const unsub = uiState.subscribe((ui) => {
     const { scrollTop, scrollBottom } = ui
     viewport = [scrollTop, scrollBottom]
   })
+  const asPlaces = (items: any[]) => items as Place[]
+  const asFestivals = (items: any[]) => items as Festival[]
   const updateTabBody = (e) => {
-    const tab: TabNav = e.detail
+    const tab: TabData = e.detail
+    activeTab = tab
+    paging = undefined
     if (tab.id === 'place') {
-      paging = placeStore.preparePagination((place) => place.type !== 'P15')
-      setTimeout(() => {
-        // cardview 렌더링 유도
-        viewport = [...viewport]
+      placeStore.loadPlaces(undefined, province.region.regionName).then(() => {
+        paging = placeStore.preparePagination((place) => place.type !== 'P15')
+        setTimeout(() => {
+          // cardview 렌더링 유도
+          viewport = [...viewport]
+        })
+      })
+    } else if (tab.id === 'festival') {
+      festivalStore.loadFestivals(province.region.regionName).then(() => {
+        paging = festivalStore.preparePagination()
       })
     } else {
       paging = undefined
       sheetStore.clear()
     }
   }
+  const unsubTab = regionTabModel.subscribe(() => {
+    updateTabBody({ detail: regionTabModel.getActiveData() })
+  })
   onDestroy(() => {
     unsub()
+    unsubTab()
     sheetStore.clear()
   })
 </script>
 
 <section in:fade={{ duration: 150 }}>
   <div class="container">
-    <TabNavView model={tabModel} on:tab-change={updateTabBody} />
+    <TabNavView model={regionTabModel} />
     <div class="row">
-      {#if $tabModel.activeId === 'route'}
+      {#if $regionTabModel.activeId === 'route'}
         {#if journeys.length === 0}
           <div class="col-12">
             <div class="none" />
@@ -94,8 +87,8 @@
           </div>
         {/each}
       {/if}
-      {#if $tabModel.activeId === 'place'}
-        {#each $paging.places as place (place.uuid)}
+      {#if paging && $regionTabModel.activeId === 'place'}
+        {#each asPlaces($paging.items) as place (place.uuid)}
           <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
             <PlaceCardView {place} {viewport} />
           </div>
@@ -103,19 +96,42 @@
         {#if paging.hasNext()}
           <div class="more">
             <AppButton
-              text="More"
+              text="+20"
               round="8px"
-              on:click={() => paging.loadMore()}
+              on:click={() => paging.loadMore(20)}
+            />
+            <AppButton
+              text="+50"
+              round="8px"
+              on:click={() => paging.loadMore(50)}
             />
           </div>
         {/if}
       {/if}
-      {#if $tabModel.activeId === 'festival'}
-        {#each $placeStore.visibles.filter((p) => p.type === 'P15') as place (place.uuid)}
+      {#if paging && $regionTabModel.activeId === 'festival'}
+        {#each asFestivals($paging.items) as festival (festival.place.uuid)}
           <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4">
-            <PlaceCardView {place} {viewport} />
+            <PlaceCardView
+              place={festival.place}
+              duration={festival}
+              {viewport}
+            />
           </div>
         {/each}
+        {#if paging.hasNext()}
+          <div class="more">
+            <AppButton
+              text="+20"
+              round="8px"
+              on:click={() => paging.loadMore(20)}
+            />
+            <AppButton
+              text="+50"
+              round="8px"
+              on:click={() => paging.loadMore(50)}
+            />
+          </div>
+        {/if}
       {/if}
     </div>
   </div>
